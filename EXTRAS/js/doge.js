01f9955aa8cdf410f0f56922598ba613e2453f0f
vjordanmc <vjordanmc@gmail.com>
2020-03-09 13:22:22 -0300

    .

--- EXTRAS/js/doge.js
+++ EXTRAS/js/doge.js
@@ -1,6 +1,15 @@
 function doge(){
     let body = document.getElementById("body");
+    let doge = document.createElement("div");
     let modal = document.createElement("div");
-    modal.id = "modal"
-    body.appendChild(modal)
+    let buttonClose = document.createElement("div");
+    doge.id = "doge"
+    modal.id = "modal";
+    buttonClose.id = "buttonClose";
+    modal.appendChild(doge);
+    modal.appendChild(buttonClose);
+    body.appendChild(modal);
+    document.getElementById('buttonClose').onclick = function(){
+        document.getElementById("body").removeChild(modal);
+    };
 }
