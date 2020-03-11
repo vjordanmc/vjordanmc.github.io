9f69c68d9ceff01ef939bc451f4a8bf46a3f55c6
vjordanmc <vjordanmc@gmail.com>
2020-03-10 21:36:40 -0300

    close_on_modal_click

--- EXTRAS/js/doge.js
+++ EXTRAS/js/doge.js
@@ -2,14 +2,20 @@ function doge(){
     let body = document.getElementById("body");
     let doge = document.createElement("div");
     let modal = document.createElement("div");
+    let modalClose = document.createElement("div");
     let buttonClose = document.createElement("div");
-    doge.id = "doge"
-    modal.id = "modal";
+    doge.id        = "doge"
+    modal.id       = "modal";
+    modalClose.id  = "modalClose"
     buttonClose.id = "buttonClose";
     modal.appendChild(doge);
+    modal.appendChild(modalClose);
     modal.appendChild(buttonClose);
     body.appendChild(modal);
     document.getElementById('buttonClose').onclick = function(){
+        document.getElementById("body").removeChild(modal);
+    };
+    document.getElementById('modalClose').onclick = function(){
         document.getElementById("body").removeChild(modal);
     };
 }
